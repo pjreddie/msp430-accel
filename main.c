6c07831398504432845bfaf1ea0bb1b07c86ae04
Joseph Redmon <pjreddie@crunchbang.(none)>
2014-06-03 23:41:07 -0700

    Sort of works now

--- main.c
+++ main.c
@@ -9,30 +9,29 @@
 #define BAUD_RATE 9600    /* Max usuable with Launchpad */
 #define XMIT_PIN BIT1     /* defaults to PORT1 */
 
-#define SAMPLES 50
+#define SAMPLES 25
+#define abs(x) ((x)<0? -(x) : (x))
 
-int8_t xyz[SAMPLES*3];
-unsigned int index = 0;
+int8_t train[SAMPLES*3];
+int8_t test[SAMPLES*3];
 uint8_t data_ready = 0;
-uint8_t button_press = 0;
+int state = 0;
+int thresh = 0;
 
-int count = 0;
 
 #pragma vector=PORT1_VECTOR
 __interrupt void Port_1(void)
 {
     if(P1IFG & BIT3){
-        button_press = 1;
+        P1IE  &= ~BIT3;
         P1IFG &= ~BIT3;
-        P1OUT ^=  BIT0;
+        __bic_SR_register_on_exit(LPM3_bits);
     }
     if(P1IFG & BIT4){
-        ++count;
         P1IES ^= BIT4;
         P1IFG &= ~BIT4;
-        data_ready = 1;
+        __bic_SR_register_on_exit(LPM3_bits);
     }
-    __bic_SR_register_on_exit(LPM3_bits); // exit active if prompted to
 }
 
 void setup_interrupts(void)
@@ -40,7 +39,6 @@ void setup_interrupts(void)
     P1SEL &= ~BIT4;
     P1DIR &= ~BIT4;
     P1IFG &= ~BIT4;
-    P1IE  |=  BIT4;
 
     P1SEL &= ~BIT3;
     P1DIR &= ~BIT3;
@@ -50,6 +48,24 @@ void setup_interrupts(void)
     P1IE  |=  BIT3;
 
     _BIS_SR(GIE);
+}
+
+void sample_xyz(void);
+void sample_xyz()
+{
+    uint8_t *buff = (uint8_t*)((state == 0)?train:test);
+    int i;
+    P1IE |= BIT4;
+    mma_active();
+    read_xyz((uint8_t *)buff);
+    P1OUT |=  BIT0;
+    for(i = 0; i < SAMPLES; ++i){
+        read_xyz((uint8_t *)&(buff[i*3]));
+        _BIS_SR(LPM3_bits + GIE);
+    }
+    P1IE &= ~BIT4;
+    mma_standby();
+    P1OUT &= ~BIT0;
 }
 
 int main(void)
@@ -64,7 +80,9 @@ int main(void)
     P1OUT |= XMIT_PIN;  // default state of TX line is high
     P1DIR |= XMIT_PIN;
     P1DIR |= BIT0;
-    P1OUT |= BIT0;
+    P1OUT &= ~BIT0;
+    P1DIR |= BIT5;
+    P1OUT &= ~BIT5;
 
     /**
      * configure serial routines for P1OUT, PIN1 9600 bps
@@ -74,28 +92,32 @@ int main(void)
 #endif
 
     mma_init();
-    uint8_t whoami = read_register(0x0d);
-    printf("Whoami %x\r\n", whoami);
-    mma_sysmod();
-    printf("before\r\n");
     setup_interrupts();
-    printf("after\r\n");
-
-    read_xyz((uint8_t *)xyz);
-    read_xyz((uint8_t *)xyz);
 
     while(1){
-        if(data_ready){
-            data_ready = 0;
-            read_xyz((uint8_t *)&xyz[index*3]);
-            ++index;
-            if(index == SAMPLES) index = 0;
+        _BIS_SR(LPM3_bits + GIE);
+        sample_xyz();
+        int i;
+        if(state > 0){
+            int diff = 0;
+            for(i = 0; i < SAMPLES*3; ++i){
+                diff += abs(train[i] - test[i]);
+            }
+            if(state == 1){
+                thresh = diff + (diff>>3);
+                printf("Difference: %d, Threshold: %d\r\n", diff, thresh);
+            }
+            else if(diff < thresh){
+                P1OUT |= BIT5;
+                printf("Unlocked! %d\r\n", diff);
+            }
+            else{
+                P1OUT &= ~BIT5;
+                printf("DENIED: %d\r\n", diff);
+            }
         }
-        if(button_press){
-            printf("Count: %d, Index: %d\r\n", count, index);
-            button_press = 0;
-        }
-        if(!data_ready) _BIS_SR(LPM3_bits + GIE);
+        if(state < 2) ++state;
+        P1IE  |=  BIT3;
     }
     while(1);
 
